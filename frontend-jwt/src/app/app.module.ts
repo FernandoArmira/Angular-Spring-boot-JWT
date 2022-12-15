34d0c90dfab7dc9b6c1c7986419e6317766bfb37
fernando.armira <2864934620101@ingenieria.usac.edu.gt>
2022-12-14 18:02:43 -0600

    Iniciando frontend

---
+++ frontend-jwt/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { UserComponent } from './user/user.component';
+import { HeaderComponent } from './header/header.component';
+import { ForbiddenComponent } from './forbidden/forbidden.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AdminComponent,
+    UserComponent,
+    HeaderComponent,
+    ForbiddenComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
